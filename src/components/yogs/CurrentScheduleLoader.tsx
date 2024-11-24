dd6f374fe848fa19b6a4dec576ec726116e95b02
Johann Feser <johannfeser@gmail.com>
2024-11-24 22:44:22 +0100

    improve disclaimer

--- src/components/yogs/CurrentScheduleLoader.tsx
+++ src/components/yogs/CurrentScheduleLoader.tsx
@@ -4,9 +4,17 @@ import {useYogsScheduleFirestore} from "../../lib/useYogsScheduleFirestore.ts";
 import type {FullCreator, FullSchedule} from "../../lib/model/ContentTypes.ts";
 import {PlaceholderSchedule} from "../schedulePlaceholder/PlaceholderSchedule.tsx";
 import {useConfig} from "../../lib/useConfig.ts";
-import {Disclaimer} from "../Disclaimer.tsx";
 import {Countdown} from "../Countdown.tsx";
 import {MobileYogsScheduleComponent} from "./schedule/mobile/MobileYogsScheduleComponent.tsx";
+import {
+  DiscordIcon,
+  GlobeIcon,
+  InstagramIcon,
+  TiktokIcon,
+  TwitchIcon,
+  TwitterIcon,
+  YoutubeIcon
+} from "../common/JJIcons.tsx";
 
 
 interface CurrentScheduleLoaderProps {
@@ -62,18 +70,7 @@ const Body: Component<BodyProps> = (props) => {
               minute: 'numeric',
               timeZoneName: 'short'
             })}</p>
-            <div class={'max-w-[30rem] text-white text-center text-pretty'}>
-              <p>
-                This site <strong>is maintained by the community</strong>.
-              </p>
-              <p>
-                If you find errors or see that a stream is missing use the contact info below or message @ostof on discord. Streams that appear here are not guaranteed to
-                happen or might be delayed. For more information visit the Jingle Jam's social media.
-              </p>
-              <p>
-                This site is a <strong>fan Project and not associated with the Jingle Jam, the Yogscast or their partners.</strong>
-              </p>
-            </div>
+            <Disclaimer/>
           </div>
           <div class="mobile-schedule w-full flex flex-col items-center justify-center">
             <MobileYogsScheduleComponent
@@ -88,20 +85,7 @@ const Body: Component<BodyProps> = (props) => {
               minute: 'numeric',
               timeZoneName: 'short'
             })}</p>
-            <div class={'max-w-[30rem] text-white text-center text-pretty'}>
-              <p>
-                This site <strong>is maintained by the community</strong>.
-              </p>
-              <p>
-                If you find errors or see that a stream is missing use the contact info below or message @ostof on
-                discord. Streams that appear here are not guaranteed to
-                happen or might be delayed. For more information visit the Jingle Jam's social media.
-              </p>
-              <p>
-                This site is a <strong>fan Project and not associated with the Jingle Jam, the Yogscast or their
-                partners.</strong>
-              </p>
-            </div>
+            <Disclaimer/>
           </div>
         </>
       </Match>
@@ -112,4 +96,73 @@ const Body: Component<BodyProps> = (props) => {
       </Match>
     </Switch>
   );
+}
+
+
+const Disclaimer: Component = () => {
+  return (
+    <div class={'max-w-[30rem] text-white text-center text-pretty'}>
+      <p>
+        This site <strong>is maintained by the community</strong>.
+      </p>
+      <p>
+        If you find errors or see that a stream is missing use the contact info below or message @ostof on
+        discord.
+      </p>
+      <p>
+        The schedule is subject to change. Not all participants are confirmed and some streams might be moved or cancelled.
+      </p>
+      <p>
+        For more information visit the Yogscast's social media.
+      </p>
+      <ExternalLinks/>
+      <p>
+        This site is a <strong>fan Project and not associated with the Jingle Jam, the Yogscast or their
+        partners.</strong>
+      </p>
+    </div>
+  )
+}
+
+
+const ExternalLinks = () => {
+  return (
+    <div class={'w-full flex flex-row gap-4 items-center justify-center p-2'}>
+      <a
+        class={'hover:scale-110 transition-all'}
+        href={'https://www.youtube.com/yogscast'}
+        target={'_blank'}
+        aria-label={'Yogscast Youtube channel'}>
+        <YoutubeIcon class={'~w-2/4 ~h-2/4'}/>
+      </a><a
+        class={'hover:scale-110 transition-all'}
+        href={'https://twitch.tv/yogscast'}
+        target={'_blank'}
+        aria-label={'Yogscast Twitch channel'}>
+        <TwitchIcon class={'~w-2/4 ~h-2/4'}/>
+      </a>
+      <a
+        class={'hover:scale-110 transition-all'}
+        target={'_blank'}
+        href={'https://x.com/yogscast'}
+        aria-label={'Yogscast Twitter'}>
+        <TwitterIcon class={'~w-2/4 ~h-2/4'}/>
+      </a>
+      <a
+        class={'hover:scale-110 transition-all'}
+        target={'_blank'}
+        href={'https://www.instagram.com/officialyogscast/'}
+        aria-label={'Yogscast Instagram'}>
+        <InstagramIcon class={'~w-2/4 ~h-2/4'}/>
+      </a>
+      <a
+        class={'hover:scale-110 transition-all'}
+        target={'_blank'}
+        href={'https://www.tiktok.com/@yogscastofficial'}
+        aria-label={'Jingle Jam Tiktok'}>
+        <TiktokIcon class={'~w-2/4 ~h-2/4'}/>
+      </a>
+    </div>
+
+  )
 }
