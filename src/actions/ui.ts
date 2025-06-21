ad2063d7faff868e5987eda336cef368e18c170e
Johann Feser <johannfeser@gmail.com>
2025-06-21 22:50:00 +0200

    Update ui.ts

--- src/actions/ui.ts
+++ src/actions/ui.ts
@@ -52,6 +52,58 @@ export const ui = {
         handler: (_, context) => {
           return fullJJExampleSchedule()
         }
+      }),
+      getCurrentPrimaryBySlug: defineAction({
+        input: z.string(),
+        handler: async (slug: string, ctx) => {
+          try {
+            const repo = ScheduleUIRepo.action(ctx);
+            const schedule = await repo.getCurrentPrimaryBySlug(slug);
+
+            if (!schedule) {
+              throw new ActionError({code: 'NOT_FOUND', message: 'Primary schedule not found for this user.'});
+            }
+
+            if (!schedule.schedule) {
+              throw new ActionError({code: 'NOT_FOUND', message: 'Schedule not found.'});
+            }
+
+            if (!schedule.schedule.visible) {
+              throw new ActionError({code: 'NOT_FOUND', message: 'Schedule is set to private'});
+            }
+
+            return schedule;
+          } catch (e: any) {
+            console.error('Error getting primary schedule by slug:', e);
+            throw new ActionError({code: 'INTERNAL_SERVER_ERROR', message: e.message});
+          }
+        }
+      }),
+      getCurrentPrimaryByTiltifyUsername: defineAction({
+        input: z.string(),
+        handler: async (tiltifyUsername: string, ctx) => {
+          try {
+            const repo = ScheduleUIRepo.action(ctx);
+            const schedule = await repo.getCurrentPrimaryByTiltifyUsername(tiltifyUsername);
+
+            if (!schedule) {
+              throw new ActionError({code: 'NOT_FOUND', message: 'Primary schedule not found for this user.'});
+            }
+
+            if (!schedule.schedule) {
+              throw new ActionError({code: 'NOT_FOUND', message: 'Schedule not found.'});
+            }
+
+            if (!schedule.schedule.visible) {
+              throw new ActionError({code: 'NOT_FOUND', message: 'Schedule is set to private'});
+            }
+
+            return schedule;
+          } catch (e: any) {
+            console.error('Error getting primary schedule by tiltify username:', e);
+            throw new ActionError({code: 'INTERNAL_SERVER_ERROR', message: e.message});
+          }
+        }
       })
     }
   }
