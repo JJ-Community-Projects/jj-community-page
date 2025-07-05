86ecf5f212c3317154c8e8d5a3689736a248e823
Johann Feser <johannfeser@gmail.com>
2025-07-06 00:15:05 +0200

    Update Repos.ts

--- src/lib/db/repos/Repos.ts
+++ src/lib/db/repos/Repos.ts
@@ -1,9 +1,11 @@
 import {UserRepo} from "./UserRepo.ts";
 import {TeamRepo} from "./TeamRepo.ts";
 import {ScheduleRepo} from "./ScheduleRepo.ts";
+import {TwitchRepo} from "./TwitchRepo.ts";
 
 export type Repos = {
   users: UserRepo,
   teams: TeamRepo,
-  schedules: ScheduleRepo
+  schedules: ScheduleRepo,
+  twitch: TwitchRepo
 }
