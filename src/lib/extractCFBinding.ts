9b48ff6fc39a653d9c3fab78deae384ce95675d7
Johann Feser <johannfeser@gmail.com>
2025-07-05 21:20:54 +0200

    Update extractCFBinding.ts

--- src/lib/extractCFBinding.ts
+++ src/lib/extractCFBinding.ts
@@ -2,11 +2,13 @@ import type {APIContext} from "astro";
 import type {ActionAPIContext} from "astro:actions";
 
 export function extractCFBinding(
-  source: APIContext | ActionAPIContext | App.Locals
-) {
-  if ('locals' in source) {
-    return source.locals.runtime
+  source: APIContext | ActionAPIContext | App.Locals | Env
+): Env {
+  if ('runtime' in source) {
+    return source.runtime.env
+  }else if ('locals' in source) {
+    return source.locals.runtime.env
   } else {
-    return source.runtime
+    return source
   }
 }
