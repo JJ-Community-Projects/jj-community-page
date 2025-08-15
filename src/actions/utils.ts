6701c3246c21593cc4ec4682c0068aeb632aea84
Johann Feser <johannfeser@gmail.com>
2025-08-16 01:08:25 +0200

    delete actions

--- src/actions/utils.ts
+++
@@ -1,9 +0,0 @@
-import {ActionError, type ActionAPIContext} from "astro:actions";
-
-
-export function handleUnauthorized(context: ActionAPIContext) {
-  const {session, user} = context.locals
-  if (!session || !user) {
-    throw new ActionError({code: 'UNAUTHORIZED'});
-  }
-}
