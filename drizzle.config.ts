1e051aaa022ee032c253f934e7259905c996854f
Johann Feser <johannfeser@gmail.com>
2025-08-19 21:16:46 +0200

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   out: './drizzle',
-  schema: './src/lib/db/schema/',
+  schema: './src/lib/db/schema/schema.ts',
   dialect: 'sqlite',
   driver: 'd1-http',
   dbCredentials: {
