8c2c11cf05272654d80222a7546dace6a82040dc
Johann Feser <johannfeser@gmail.com>
2025-10-12 18:37:05 +0200

    Update JJData.tsx

--- src/components/features/admin/JJData.tsx
+++ src/components/features/admin/JJData.tsx
@@ -1,33 +1,65 @@
-import { type Component } from 'solid-js'
-import { orpcPrivate, orpcPublic } from '../../../lib/orpc/client.ts'
+import { type Component, createEffect, on, Show } from 'solid-js'
+import { orpcPrivate } from '../../../lib/orpc/client.ts'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/solid-query'
 
 export const JJData: Component = () => {
   const client = useQueryClient()
-  const jjData = orpcPublic.jjData
+  const jjData = orpcPrivate.jj
   const admin = orpcPrivate.admin
 
   const refresh = useMutation(() =>
     admin.refreshJJAPIData.mutationOptions({
-      onSuccess: () => {
-        client.invalidateQueries({
+      onSuccess: async () => {
+        await client.invalidateQueries({
           queryKey: jjData.causes.queryKey(),
         })
-        client.invalidateQueries({
-          queryKey: jjData.causes.queryKey(),
-        })
+        /*
+        await client.invalidateQueries({
+          queryKey: jjData.campaigns.queryKey(),
+        }*/
       },
     }),
   )
 
   const causes = useQuery(() => jjData.causes.queryOptions())
-  const campaigns = useQuery(() => jjData.campaigns.queryOptions())
+  // const campaigns = useQuery(() => jjData.campaigns.queryOptions())
+
+  createEffect(
+    on(
+      () => causes.data,
+      (d) => {
+        console.log('causes.data', d)
+      },
+    ),
+  )
+  createEffect(
+    on(
+      () => causes.error,
+      (d) => {
+        console.log('causes.error', d)
+      },
+    ),
+  )
 
   return (
-    <div class="flex flex-col">
-      <button onClick={refresh.mutate}>Refresh</button>
-      <pre>{JSON.stringify(causes?.data, null, 2)}</pre>
-      <pre>{JSON.stringify(campaigns?.data, null, 2)}</pre>
+    <div class="flex flex-col text-black">
+      <button class={'bg-accent text-white'} onClick={refresh.mutate}>
+        Refresh
+      </button>
+      <p>{causes.status}</p>
+      <Show when={causes.error}>
+        {(e) => <p>{JSON.stringify(e, null, 2)}</p>}
+      </Show>
+      <Show when={causes.data}>
+        {(data) => {
+          return (
+            <>
+              <p>Causes:</p>
+              <pre>{JSON.stringify(data(), null, 2)}</pre>
+            </>
+          )
+        }}
+      </Show>
     </div>
   )
 }
