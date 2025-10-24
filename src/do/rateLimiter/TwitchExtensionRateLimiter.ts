8c195eec56f74724931da4699629c33d7326a1cd
Johann Feser <johannfeser@gmail.com>
2025-10-24 12:36:27 +0200

    Update TwitchExtensionRateLimiter.ts

--- src/do/rateLimiter/TwitchExtensionRateLimiter.ts
+++ src/do/rateLimiter/TwitchExtensionRateLimiter.ts
@@ -3,8 +3,8 @@ import { RateLimiter, type RateLimiterOptions } from './RateLimiter.ts'
 export class TwitchExtensionRateLimiter extends RateLimiter {
   protected getOptions(): RateLimiterOptions {
     return {
-      capacity: 20, // allow up to 20 tokens burst
-      refillTokens: 20, // add 20 tokens
+      capacity: 100, // allow up to 20 tokens burst
+      refillTokens: 100, // add 20 tokens
       refillIntervalMs: 60_000, // every 60 seconds → average 20 requests/min after burst
       gracePeriodMs: 5000, // small grace
     }
