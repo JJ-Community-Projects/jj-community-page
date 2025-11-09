e23cdc3816923a9671c342ec7d5561ea66631c57
Johann Feser <johannfeser@gmail.com>
2025-11-09 15:31:01 +0100

    update z index

--- src/components/layout/navigation/YogsScheduleNavigation.tsx
+++ src/components/layout/navigation/YogsScheduleNavigation.tsx
@@ -1,5 +1,4 @@
-import {getCollection} from "astro:content";
-import {type Component} from "solid-js";
+import { type Component } from 'solid-js'
 
 interface YogsScheduleNavigationProps {
     paths: string[]
@@ -7,7 +6,7 @@ interface YogsScheduleNavigationProps {
 
 export const YogsScheduleNavigation: Component<YogsScheduleNavigationProps> = (props) => {
     return (
-      <div class="absolute z-10 hidden w-30 group-hover:block">
+      <div class="absolute z-50 hidden w-30 group-hover:block">
           <div class="bg-accent w-full rounded px-2 pb-4 pt-2 shadow-lg">
               <div class="flex flex-col gap-4 text-center md:grid-cols-2">
                   <a class="nav" href={`/yogs`}>Yogs</a>
