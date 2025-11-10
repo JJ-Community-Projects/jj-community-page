b06df4fe06346cc62d555bcfeb3c0d3331d18dd2
Johann Feser <johannfeser@gmail.com>
2025-11-10 11:34:28 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,68 +1,75 @@
-import {defineConfig, passthroughImageService} from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config'
 
-import solidJs from '@astrojs/solid-js';
+import solidJs from '@astrojs/solid-js'
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from '@astrojs/cloudflare'
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
-import sitemap from '@astrojs/sitemap';
-import commonjs from 'vite-plugin-commonjs';
+import sitemap from '@astrojs/sitemap'
+import commonjs from 'vite-plugin-commonjs' // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jinglejam.ostof.dev',
-    security: {
-        checkOrigin: true
-    },
-    integrations: [solidJs(), tailwind(), sitemap(
-        {
-            filter: (page) => page !== 'https://jinglejam.ostof.dev/login/' &&
-                page !== 'https://jinglejam.ostof.dev/faq/' &&
-                page !== 'https://jinglejam.ostof.dev/about/' &&
-                page !== 'https://jinglejam.ostof.dev/overlays/charities/' &&
-                page !== 'https://jinglejam.ostof.dev/overlays/charities2/' &&
-                page !== 'https://jinglejam.ostof.dev/overlays/fundraisers/' &&
-                page !== 'https://jinglejam.ostof.dev/yogs/archive/404/' &&
-                page !== 'https://jinglejam.ostof.dev/yogs/privacy/twitch-extension' &&
-                !page.includes('pwa'),
-        }
-    )],
-    output: 'server',
-    adapter: cloudflare({
-        imageService: 'passthrough',
-        platformProxy: {
-            enabled: true
-        }
+  site: 'https://jinglejam.ostof.dev',
+  security: {
+    checkOrigin: true,
+  },
+  integrations: [
+    solidJs(),
+    tailwind(),
+    sitemap({
+      filenameBase: 'sitemap',
+      filter: (page) =>
+        page !== 'https://jinglejam.ostof.dev/login/' &&
+        page !== 'https://jinglejam.ostof.dev/admin/' &&
+        page !== 'https://jinglejam.ostof.dev/faq/' &&
+        page !== 'https://jinglejam.ostof.dev/about/' &&
+        !page.includes('404') &&
+        !page.includes('error') &&
+        !page.includes('/overlays/') &&
+        !page.includes('/dashboard/') &&
+        !page.includes('not-found') &&
+        !page.includes('/api/') &&
+        !page.includes('embed') &&
+        page !== 'https://jinglejam.ostof.dev/yogs/privacy/twitch-extension/' &&
+        !page.includes('pwa'),
     }),
-    image: {
-        service: passthroughImageService()
+  ],
+  output: 'server',
+  adapter: cloudflare({
+    imageService: 'passthrough',
+    platformProxy: {
+      enabled: true,
     },
-    vite: {
-        plugins: [commonjs()],
-        optimizeDeps: {
-            include: ['debug', 'extend'],
-        },
-        resolve: {
-            alias: {
-                // debug: 'debug/src/browser.js', // Force the correct ESM file
-            },
-        },
-        build: {
-            minify: false,
-        },
-    },
+  }),
+  image: {
+    service: passthroughImageService(),
+  },
+  vite: {
+    plugins: [commonjs()],
     optimizeDeps: {
-        include: ['debug', 'solid-markdown', 'extend'],
+      include: ['debug', 'extend'],
     },
     resolve: {
-        alias: {
-            debug: 'debug/src/browser.js',
-            extend: 'extend/index.js',
-
-        },
+      alias: {
+        // debug: 'debug/src/browser.js', // Force the correct ESM file
+      },
     },
     build: {
-        exclude: ['.cache', 'node_modules'],
+      minify: false,
     },
-});
+  },
+  optimizeDeps: {
+    include: ['debug', 'solid-markdown', 'extend'],
+  },
+  resolve: {
+    alias: {
+      debug: 'debug/src/browser.js',
+      extend: 'extend/index.js',
+    },
+  },
+  build: {
+    exclude: ['.cache', 'node_modules'],
+  },
+})
