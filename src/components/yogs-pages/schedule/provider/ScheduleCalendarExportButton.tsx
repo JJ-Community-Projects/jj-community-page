bb0032e174feb6929d98ae06845655455827ee27
Johann Feser <johannfeser@gmail.com>
2025-11-22 13:00:05 +0100

    Update ScheduleCalendarExportButton.tsx

--- src/components/yogs-pages/schedule/provider/ScheduleCalendarExportButton.tsx
+++ src/components/yogs-pages/schedule/provider/ScheduleCalendarExportButton.tsx
@@ -143,15 +143,15 @@ const CalendarDialogDialogBody: Component<CalendarDialogDialogBodyProps> = props
     )
   }
   return (
-    <div class={'flex h-full w-full flex-col rounded-3xl bg-white'}>
-      <div class={`bg-primary flex h-[72px] items-center justify-center rounded-t-3xl p-2 text-white shadow-xl`}>
+    <div class={'flex h-full w-full flex-col rounded-2xl bg-white'}>
+      <div class={`bg-primary flex max-h-[72px] items-center justify-center rounded-t-2xl p-2 text-white shadow-xl`}>
         <button onClick={onClose}>
-          <FaSolidXmark size={24} class={''}/>
+          <FaSolidXmark size={20} class={''}/>
         </button>
         <div class={'flex-1'}></div>
-        <h3 class={'text-2xl'}>Calendar Export</h3>
+        <h3 class={'text-xl'}>Calendar Export</h3>
         <div class={'flex-1'}></div>
-        <div class={'w-[24px]'}></div>
+        <div class={'w-[20px]'}></div>
       </div>
 
       <a
